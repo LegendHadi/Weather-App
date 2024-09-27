2ddb27c08b344ce0d36c8f45dc7ee17bea85e995
DESKTOP-SKMPL8O\Legend Hadi <mohammad.hadi.hosseini81@gmail.com>
2024-09-27 16:09:36 +0330

    Add submit function for Enter keyboard

--- logic.js
+++ logic.js
@@ -1,37 +1,45 @@
-const apiKey= "500c9d7b7987ee49fecbeb4f04feae0e";
-const apiUrl= "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const apiKey = "500c9d7b7987ee49fecbeb4f04feae0e";
+const apiUrl ="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-const searchBox= document.querySelector(".search input");
-const searchBtn= document.querySelector(".search button");
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
 
 let firstTime = true;
 
 async function checkWeather(city) {
-    const response= await fetch(apiUrl + city + `&appid=${apiKey}`);
-    if(response.status == 404){
-        document.querySelector(".error").style.display= "block";
-        document.querySelector(".weather").style.display= "none";
-    }else{
-        var data= await response.json();
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+  if (response.status == 404) {
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".weather").style.display = "none";
+  } else {
+    var data = await response.json();
 
-        document.querySelector(".city").innerHTML= data.name;
-        document.querySelector(".temp").innerHTML= Math.round(data.main.temp) + "°c";
-        document.querySelector(".humidity").innerHTML= data.main.humidity + "%";
-        document.querySelector(".wind").innerHTML= data.wind.speed + " km/h";
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML =
+      Math.round(data.main.temp) + "°c";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
 
-        document.querySelector(".weather-icon").src= `images/${data.weather[0].main}.png`;
+    document.querySelector(
+      ".weather-icon"
+    ).src = `images/${data.weather[0].main}.png`;
 
-        document.querySelector(".error").style.display= "none";
-        document.querySelector(".weather").style.display= "block";
-    }
-    
+    document.querySelector(".error").style.display = "none";
+    document.querySelector(".weather").style.display = "block";
+  }
 }
 
-if(firstTime){
-    checkWeather("kabul");
-    firstTime = !firstTime;
+if (firstTime) {
+  checkWeather("kabul");
+  firstTime = !firstTime;
 }
 
-searchBtn.addEventListener("click", ()=>{
+searchBtn.addEventListener("click", () => {
+  checkWeather(searchBox.value);
+});
+
+searchBox.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
     checkWeather(searchBox.value);
+  }
 });
